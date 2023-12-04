69d3596e61f9f0d95f9690e6cd8795c9677ccc0e
Ethan Miller <milleret@mail.gvsu.edu>
2023-12-03 22:01:56 -0500

    decision tree bot

--- MeleeBot.py
+++ MeleeBot.py
@@ -6,13 +6,18 @@ import random
 import os
 import time
 import psutil
+from pathlib import Path
 from Stop import stop
 
 
 # Assuming you're running this under Windows. Didn't want to have a CLI argument every time.
-homeDirectory = os.path.expanduser('~'+os.environ.get("USERNAME"))
+home_directory = "/Users/emiller3425"
+slippi_path = "Library/Application Support/Slippi Launcher/netplay"
+
+full_path = Path(os.path.expanduser(os.path.join(home_directory, slippi_path)))
+print(full_path)
 # Emulator config.
-console = melee.Console(path=homeDirectory+"\\AppData\\Roaming\\Slippi Launcher\\netplay", slippi_address="127.0.0.1")
+console = melee.Console(path=str(full_path.resolve()), slippi_address="127.0.0.1")
 # Bot controller config.
 controller = melee.Controller(console=console, port=1)
 # There are bugs that may prevent this from selecting a CPU player. I have figured these out but I
@@ -30,7 +35,7 @@ signal.signal(signal.SIGINT, close)
 
 # Start the emulator and connect to it. Put the game in the same directory as this file for this to work.
 # Rename the iso to match. If it's not a .nkit I don't think it really matters, just rename it anyways.
-console.run("./ssb.iso")
+console.run(iso_path="/Users/emiller3425/cis365/CIS365FinalProject/ssb.iso", exe_name="Slippi Dolphin")
 console.connect()
 
 # Connect virtual controller.
@@ -43,16 +48,54 @@ while True:
     if gamestate.menu_state in [melee.Menu.IN_GAME, melee.Menu.SUDDEN_DEATH]:
         # Game is active, logic goes here.
 
-        if gamestate.distance < 4:
-            controller.press_button(melee.Button.BUTTON_B)
-            controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 0)
-        else:
-            onleft = gamestate.player[1].x < gamestate.player[2].x
-            controller.tilt_analog(melee.Button.BUTTON_MAIN, int(onleft), 0.5)
-            controller.release_button(melee.Button.BUTTON_B)
-
+        #If agent is off stage
+        if gamestate.player[1].off_stage == 1:
+            controller.press_button(melee.Button.BUTTON_X)
+            controller.release_button(melee.Button.BUTTON_X)
+            controller.press_button(melee.Button.BUTTON_X)
+            controller.release_button(melee.Button.BUTTON_X)
+            gamestate = console.step()
+        if gamestate.player[2].off_stage == 1:
+            controller.release_all()
+        #If opponant is off stage
+        elif gamestate.distance < 6:
             if gamestate.player[1].y < gamestate.player[2].y:
                 controller.press_button(melee.Button.BUTTON_X)
+                controller.release_button(melee.Button.BUTTON_X)
+            attackType = random.randint(0, 15)
+            #random chance for heavy attack
+            if attackType > 10:
+                controller.press_button(melee.Button.BUTTON_B)
+                controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 0)
+            #random chance for light attack
+            elif attackType > 5:
+                controller.press_button(melee.Button.BUTTON_A)
+                controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 0)
+            #random chance for a light jump attack
+            else:
+                controller.tilt_analog(melee.Button.BUTTON_MAIN, int(onleft), 1)
+                controller.press_button(melee.Button.BUTTON_X)
+                controller.release_button(melee.Button.BUTTON_X)
+                controller.press_button(melee.Button.BUTTON_X)
+                controller.release_button(melee.Button.BUTTON_X)
+                controller.press_button(melee.Button.BUTTON_A)
+                controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 0)
+        else:
+            # Move towards opponent if outside attack range
+            onleft = gamestate.player[1].x < gamestate.player[2].x
+            controller.press_button(melee.Button.BUTTON_A)
+            controller.tilt_analog(melee.Button.BUTTON_MAIN, int(onleft), 0.5)
+            controller.release_button(melee.Button.BUTTON_A)
+            #If below oppanant jump above them
+            if gamestate.player[1].y < gamestate.player[2].y:
+                controller.release_all()
+                controller.tilt_analog(melee.Button.BUTTON_MAIN, int(onleft), 1)
+                controller.press_button(melee.Button.BUTTON_X)
+                controller.release_button(melee.Button.BUTTON_X)
+                controller.press_button(melee.Button.BUTTON_X)
+                controller.release_button(melee.Button.BUTTON_X)
+                controller.press_button(melee.Button.BUTTON_X)
+                controller.release_button(melee.Button.BUTTON_X)
             else:
                 controller.release_button(melee.Button.BUTTON_X)
 
@@ -61,7 +104,7 @@ while True:
         melee.MenuHelper.choose_character(melee.Character.FOX,
                                           gamestate,
                                           opponentController,
-                                          cpu_level=3,
+                                          cpu_level=9,
                                           costume=0,
                                           swag=False)
         melee.MenuHelper.menu_helper_simple(gamestate,
